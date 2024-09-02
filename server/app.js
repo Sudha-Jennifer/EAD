06a14eb6008bba30125e824639a050ff0ecbc286
Sudha-Jennifer <jennnnniferr.v@gmail.com>
2024-09-02 17:16:19 +0530

    updating

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
-const express = require('express')
+const express = require ('express')
 const mongoose = require('mongoose')
 //const cors=require('cors')
-const alienRouter = require('./routes/students')
+const studentRouter = require('./routes/students')
 
 
-const url = 'mongodb://127.0.0.1:27017/MGIT'
+//const url = 'mongodb://127.0.0.1:27017/cbit'
+//const url = 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
+const url = 'mongodb+srv://jennnnniferrv:<password>@merncurd.nppuh.mongodb.net/?retryWrites=true&w=majority&appName=merncurd';
 const app = express()
 mongoose.connect(url)
 const con = mongoose.connection
@@ -17,7 +19,7 @@ console.log('connected...')
 //app.use(cors())
 app.use(express.json())
 
-app.use('/aliens',alienRouter)
+app.use('/students',studentRouter)
 app.listen(9000, () =>
 {
 console.log('Server started')
